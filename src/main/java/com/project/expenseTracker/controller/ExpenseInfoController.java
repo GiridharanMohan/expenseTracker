9da78926cc0d64b5d594fbb343b03141d1188074
GiridharanMohan <giri28602@gmail.com>
2025-11-11 13:11:36 +0530

    Removed API responses annotation and corrected alignment

--- src/main/java/com/project/expenseTracker/controller/ExpenseInfoController.java
+++ src/main/java/com/project/expenseTracker/controller/ExpenseInfoController.java
@@ -2,21 +2,15 @@ package com.project.expenseTracker.controller;
 
 import com.project.expenseTracker.model.ExpenseInfo;
 import com.project.expenseTracker.service.ExpenseInfoService;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @Slf4j
-@CrossOrigin(origins = "http://localhost:5500")
 @RestController
 @RequestMapping("/expenses")
 public class ExpenseInfoController {
@@ -24,46 +18,39 @@ public class ExpenseInfoController {
     @Autowired
     ExpenseInfoService expenseInfoService;
 
-    @GetMapping("/")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "fetched data successfully"),
-            @ApiResponse(responseCode = "404", description = "Not found", content = @Content(schema = @Schema()))})
+    @GetMapping(value = "/")
     public ResponseEntity<List<ExpenseInfo>> getAllExpense() {
         return expenseInfoService.getAllExpense();
     }
 
-    @GetMapping("/{id}")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "successfully retrieved data", content = @Content(schema = @Schema(implementation = ExpenseInfo.class))),
-            @ApiResponse(responseCode = "404", description = "Not found", content = @Content(schema = @Schema()))})
+    @GetMapping(value = "/{id}")
     public ResponseEntity<ExpenseInfo> getExpense(@PathVariable UUID id) {
         return expenseInfoService.getExpense(id);
     }
 
-    //paginated
-//    @GetMapping("/page")
-//    public ResponseEntity<Page<ExpenseInfo>> getExpenseByPage(@RequestParam int page, @RequestParam int size) {
-//        return expenseInfoService.getAllExpenseInPages(page, size);
-//    }
-
-    @PostMapping("/add")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Expense created successfully"),
-            @ApiResponse(responseCode = "400", description = "Bad request, Invalid value!!", content = @Content(schema = @Schema()))})
+    @PostMapping(value = "/add")
     public ResponseEntity<String> addExpense(@Valid @RequestBody ExpenseInfo expense) {
         log.info("Expense: {}", expense);
         return expenseInfoService.addExpense(expense);
     }
 
-    @PutMapping("/update/{id}")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "updated successfully"),
-            @ApiResponse(responseCode = "400", description = "Bad request, Invalid value!!", content = @Content(schema = @Schema()))})
+    @PutMapping(value = "/update/{id}")
     public ResponseEntity<String> updateExpense(@Valid @RequestBody ExpenseInfo expense, @PathVariable UUID id) {
         log.info("Expense: {}, ID: {}", expense, id);
         return expenseInfoService.updateExpense(expense, id);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<String> deleteExpense(@PathVariable UUID id) {
         log.info("ID: {}", id);
         return expenseInfoService.deleteExpense(id);
     }
+
+
+//    paginated
+//    @GetMapping("/page")
+//    public ResponseEntity<Page<ExpenseInfo>> getExpenseByPage(@RequestParam int page, @RequestParam int size) {
+//        return expenseInfoService.getAllExpenseInPages(page, size);
+//    }
 
 }
