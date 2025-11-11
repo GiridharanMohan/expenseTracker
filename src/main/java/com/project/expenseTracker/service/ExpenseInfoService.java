1bafefb77a824f6ac33b6dd97cb493bf3f7ec91c
GiridharanMohan <giri28602@gmail.com>
2025-11-11 13:06:00 +0530

    commented pagination method

--- src/main/java/com/project/expenseTracker/service/ExpenseInfoService.java
+++ src/main/java/com/project/expenseTracker/service/ExpenseInfoService.java
@@ -4,9 +4,6 @@ import com.project.expenseTracker.model.ExpenseInfo;
 import com.project.expenseTracker.repository.ExpenseInfoRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -77,7 +74,6 @@ public class ExpenseInfoService {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Expense with ID: " + id + " not found");
         }
     }
-
 
 
 //    paginated
