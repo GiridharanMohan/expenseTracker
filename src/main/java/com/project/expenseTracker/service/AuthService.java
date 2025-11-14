7f857264143798de8b7eb355b3de75336112f7d3
GiridharanMohan <giri28602@gmail.com>
2025-11-14 20:56:20 +0530

    feat: added email sender service on new user registration

--- src/main/java/com/project/expenseTracker/service/AuthService.java
+++ src/main/java/com/project/expenseTracker/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.project.expenseTracker.service;
 
+import com.project.expenseTracker.model.EmailDetails;
 import com.project.expenseTracker.model.User;
 import com.project.expenseTracker.repository.UserRepo;
+import com.project.expenseTracker.util.EmailSender;
 import com.project.expenseTracker.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class AuthService {
 
@@ -24,15 +28,21 @@ public class AuthService {
     @Autowired
     private JwtUtil jwtUtil;
 
+    @Autowired
+    private EmailSender emailSender;
+
 
     public ResponseEntity<?> registerUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        if(userRepo.findByEmail(user.getEmail()).isPresent()){
+        if (userRepo.findByEmail(user.getEmail()).isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
         }
         userRepo.save(user);
         String token = jwtUtil.generateToken(user.getEmail());
-        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "User Registered successfully","token",token));
+        //email generator
+        emailSender.accountCreationEmailSender(user.getEmail(), user.getUsername());
+        log.info("new user added mail sent to user : {}", user.getUsername());
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "User Registered successfully", "token", token));
     }
 
 
@@ -41,16 +51,15 @@ public class AuthService {
         String email = credentials.get("email");
 
         Optional<User> user = userRepo.findByEmail(email);
-        if(user.isEmpty()){
+        if (user.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found, please sign up");
         }
 
-        //user.get().getPassword().equals(password)
-        if(!passwordEncoder.matches(password, user.get().getPassword())){
+        if (!passwordEncoder.matches(password, user.get().getPassword())) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid password");
         }
 
         String token = jwtUtil.generateToken(email);
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("token",token));
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("token", token));
     }
 }
